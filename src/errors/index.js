584bd08aa39ff9433e86e80373ccb412e6175e1d
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2024-03-20 22:13:17 -0400

    Cleaned up

--- src/errors/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BrevianApiTimeoutError = exports.BrevianApiError = void 0;
-var BrevianApiError_1 = require("./BrevianApiError");
-Object.defineProperty(exports, "BrevianApiError", { enumerable: true, get: function () { return BrevianApiError_1.BrevianApiError; } });
-var BrevianApiTimeoutError_1 = require("./BrevianApiTimeoutError");
-Object.defineProperty(exports, "BrevianApiTimeoutError", { enumerable: true, get: function () { return BrevianApiTimeoutError_1.BrevianApiTimeoutError; } });
