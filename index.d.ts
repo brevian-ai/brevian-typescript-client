addcbb9fc0f0e0fbcbe451550e059ddf17d63409
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2024-03-21 01:19:41 -0400

    remove

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export * as BrevianApi from "./api";
-export { BrevianApiClient } from "./Client";
-export { BrevianApiError, BrevianApiTimeoutError } from "./errors";
-//# sourceMappingURL=index.d.ts.map
